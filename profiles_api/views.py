1e7869c1ea5c0d00cc61e80b2f39b40b3d5e1131
Taushif786 <taushif1teza@gmail.com>
2023-09-08 15:50:52 +0545

    Added login api and generate Token for user

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from .serializers import HelloSerializer, UserProfileSerializer
 from .models import UserProfile
@@ -118,3 +120,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         "name",
         "email",
     )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication token"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
